b526144bf2cbb45b67f21b87ffec2d4a37b6860c
Asutin12 <asuto12271818@gmail.com>
2022-03-31 09:27:31 +0900

    stateの更新

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import React, { useState } from 'react';
  */
  export const App = () => {
   const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+  console.log(setDogUrl);
     return (
       <header>
         <h1>Lumiel</h1>
         <p>犬の画像を表示するサイトです。</p>
         <img src={dogUrl} alt='犬の画像'></img>
+        <button onClick={()=> setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>更新</button>
       </header>
    )
   
