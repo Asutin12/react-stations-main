4f2f556cc1167f2543a06329536a27127e9dbbb7
Asutin12 <asuto12271818@gmail.com>
2022-04-01 16:39:06 +0900

    API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // DO NOT DELETE
-
 import * as React from 'react';
 import { render } from 'react-dom';
 import './App.css';
@@ -10,27 +9,14 @@ import React, { useState } from 'react';
  */
  export const App = () => {
   const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
-  const upDateImage=()=>{
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res=>res.json())
-    .then(
-      (result) => {
-        setDogUrl(()=>{
-          result.message
-        });
-      },
-      (error)=>{
-        console.log(error);
-      }
-    )
-  }
-  console.log(setDogUrl);
+
+  
     return (
       <header>
         <h1>Lumiel</h1>
         <p>犬の画像を表示するサイトです。</p>
         <img src={dogUrl} alt='犬の画像'></img>
-        <button onClick={()=> App}>更新</button>
+        <button onClick={upDateImage}>更新</button>
       </header>
    )
 }
