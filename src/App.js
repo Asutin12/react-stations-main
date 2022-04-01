6aa0ab0f9814cd24cb3d6bc0eb6096503eb627f3
Asutin12 <asuto12271818@gmail.com>
2022-04-01 17:05:12 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import './App.css';
-import React, { useState } from 'react';
 /**
  * 
  * @type {React.FC}
  */
  export const App = () => {
-  const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
-
-  
-    return (
+  return (
       <header>
         <h1>Lumiel</h1>
         <p>犬の画像を表示するサイトです。</p>
-        <img src={dogUrl} alt='犬の画像'></img>
-        <button onClick={upDateImage}>更新</button>
       </header>
    )
 }
